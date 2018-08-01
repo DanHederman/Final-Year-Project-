53fda82c4f3acaf0755600f7cbf5f9225197d49d
Dan Hederman <c15410232@mydit.ie>
2018-08-01 15:54:46 +0100

    Work on database
    Database work not testable as a raspberry pi is required. May implement a local host on local machine

--- Scanner/app/src/main/java/com/abc/scanner/SignUp.java
+++ Scanner/app/src/main/java/com/abc/scanner/SignUp.java
@@ -1,13 +1,64 @@
 package com.abc.scanner;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.URL;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText full_name, email, password, passwordconfo;
+
+    String FullName, Password, PasswordConfo, Email;
+
+    Context ctx = this;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        full_name = (EditText) findViewById(R.id.fullname);
+        password = (EditText) findViewById(R.id.pwd);
+        passwordconfo = (EditText) findViewById(R.id.pwdconfo);
+        email = (EditText) findViewById(R.id.email);
+    }
+
+    public void register(View V){
+
+        FullName = full_name.getText().toString();
+        Password = password.getText().toString();
+        PasswordConfo = passwordconfo.getText().toString();
+        Email = email.getText().toString();
+
+
+
+        BackGround b = new BackGround();
+
+    }
+
+    class BackGround extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String full_name = params[0];
+            String email = params[2];
+            String password = params[1];
+            String data ="";
+            int tmp;
+
+            try{
+                URL url = new URL()
+            }
+
+            return null;
+        }
     }
 }
